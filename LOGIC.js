77b3ebf855a8b454d8c5f723e5e3d67a5728dfda
Sam <samdehority@outlook.com>
2022-07-24 20:53:33 -0400

    Adding all files from desktop

---
+++ LOGIC.js
@@ -0,0 +1,25 @@
+document.getElementById("emailAddress").addEventListener("click", copyEmailAddress);
+
+function copyEmailAddress (){
+  window.prompt(`Copy my email address and send me a note. Use "Ctrl + C" to copy then "Enter" to close this prompt.`, `sdehority1@gmail.com`);
+}
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
